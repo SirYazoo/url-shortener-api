896f9c16a8ea3992a2b013ac232b15955c08088b
SirYazoo <rioaurelio25@gmail.com>
2026-04-03 12:40:17 +0700

    feat: implement URL shortening logic and in-memory storage

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const PORT = 3000;
 app.use(express.json());
 
 app.post("/api/shorten", (req, res) => {
-  console.log("Shorten endpoint was hit!");
-  res.status(200).json({ status: "OK", message: "Route is wired up" });
+  const longUrl = req.body.longUrl;
+  if (!longUrl) {
+    return res.status(400).json({ message: "longUrl is required" });
+  }
+  const shortCode = Math.random().toString(36).substring(2, 8);
+  urlDatabase[shortCode] = longUrl;
+  console.log(urlDatabase);
+  res.status(201).json({
+    message: "URL shortened successfully",
+    url: `http://localhost:${PORT}/${shortCode}`,
+  });
 });
 
 app.get("/:shortCode", (req, res) => {
