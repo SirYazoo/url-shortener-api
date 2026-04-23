9fd28b87bcc0ef1307cd77c6eda25ef6beae82e3
SirYazoo <rioaurelio25@gmail.com>
2026-04-23 15:17:37 +0700

    feat: implement API protection mechanisms
    - Integrated express-rate-limit to prevent database spamming and DDoS attempts on the POST route.
    - Implemented native Node.js URL constructor for strict data validation on incoming links.
    - Hardened API against malformed payloads and malicious actors.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import express from "express";
+import rateLimit from "express-rate-limit";
 import { PrismaClient } from "./generated/prisma/client.ts";
+
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
@@ -9,13 +11,37 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+const shortenLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15,
+  message: {
+    success: false,
+    message:
+      "Too many URLs created from this IP. Please try again after 15 minutes.",
+  },
+});
 
-app.post("/api/shorten", async (req, res, next) => {
+const isValidUrl = (urlString) => {
+  if (!URL.canParse(urlString)) return false;
+
+  const url = new URL(urlString);
+  return url.protocol === "http:" || url.protocol === "https:";
+};
+
+app.post("/api/shorten", shortenLimiter, async (req, res, next) => {
   try {
     const { longUrl, customCode } = req.body;
     if (!longUrl) {
       return res.status(400).json({ message: "longUrl is required" });
     }
+
+    if (!isValidUrl(longUrl)) {
+      return res.status(400).json({
+        message:
+          "Invalid URL format. Must be a valid http:// or https:// link.",
+      });
+    }
+
     let shortCode;
     if (customCode) {
       const sanitizedCode = customCode
@@ -36,12 +62,14 @@ app.post("/api/shorten", async (req, res, next) => {
     } else {
       shortCode = Math.random().toString(36).substring(2, 8);
     }
+
     await prisma.url.create({
       data: {
         shortCode: shortCode,
         longUrl: longUrl,
       },
     });
+
     res.status(201).json({
       success: true,
       message: "URL shortened successfully",
@@ -60,9 +88,11 @@ app.get("/:shortCode", async (req, res, next) => {
         shortCode: shortCode,
       },
     });
+
     if (!urlRecord) {
       return res.status(404).json({ message: "URL not found" });
     }
+
     const longUrl = urlRecord.longUrl;
     await prisma.url.update({
       where: {
@@ -72,6 +102,7 @@ app.get("/:shortCode", async (req, res, next) => {
         clicks: { increment: 1 },
       },
     });
+
     res.redirect(longUrl);
   } catch (error) {
     next(error);
